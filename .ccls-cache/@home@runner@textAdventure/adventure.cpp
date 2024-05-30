135cff82ee8c55f58e6ae448b4abe69693a95489
bluejazx <108899308+bluejazx@users.noreply.github.com>
2024-05-30 16:30:41 +0000

    Moved code into funtion
    To-Do:
    -find out how to avoid infinite loop cause by enerting something other than an integer

--- .ccls-cache/@home@runner@textAdventure/adventure.cpp
+++ .ccls-cache/@home@runner@textAdventure/adventure.cpp
@@ -1,4 +1,60 @@
 #include <iostream>
+#include <vector>
+
+//totals the players stats
+int stat_total(int strength, int perception, int endurance, int charisma, int intellegience, int agility, int luck )
+{
+  int total = (strength + perception + endurance + charisma + intellegience + agility + luck );
+  return total;
+}
+
+//allows the player to name things
+std::string name()
+{
+  std::string new_name;
+  std::cout << "Enter the name you desire: \n";
+  std::cin >> new_name;
+  return new_name;
+}
+
+// gets the player choice out of a numbere list 
+int player_option()
+{
+  int player_Answer;
+  std::cout << "Enter the number of the option you choose: \n";
+  std::cin >> player_Answer;
+  return player_Answer;
+}
+
+//Prints menu
+void print_menu(std::vector <std::string> menu_options)
+{
+  //prints each item stored in menu vector
+  for(int i = 0;  i < menu_options.size(); i++)
+    {
+      std::cout << menu_options[i];
+    }
+}
+
+//Prints basic information
+void help()
+{
+  std::cout << "This is a text based adevture game where you enter the number of choice you wish to select. \n";
+  std::cout << "The only ting saved betwenn adventure is the gold you had upon your return and the unlocks you had. \n";
+  std::cout << "If you are finshed reading enter 1 to continue back to the orginal menu";
+}
+
+//Ask the player what mode the want to play on
+void what_mode()
+{
+  std::cout << "How Do you wish to adventure: \n";
+}
+
+//Generic error message
+void error()
+{
+  std::cout << "It appears you have made an incorrect entry please tey agian";
+}
 
 int main()
 {
@@ -12,6 +68,8 @@ int main()
   bool adventure = true;
   //set to true triggering adventure when player says they want to do infinite adventure
   bool infinite;
+  //stores the choice the player gives when prompted 
+  int choice;
   
   //For quest mode only
   //set to true triggering adventure when player says they want to quest
@@ -28,7 +86,6 @@ int main()
   int intellegience;
   int agility;
   int luck;
-  int statTotal =(strength + perception + endurance + charisma + intellegience + agility + luck );
 
   //Theplayer chracter choice influences starting items
   std::string playerCharacter;
@@ -40,94 +97,67 @@ int main()
   //tracks player score and high score
   int playerscore;
   int highScore;
-  
+
+  //Vectors of Menus
   //Main Menu
-  //ask the player what kind of adventure they want to embark on infinite or quest
-  std::cout << "How Do you wish to adventure: \n";
-  std::cout << " 1.Endless\n";
-  std::cout << " 2.Quest\n";
+  std::vector <std::string> modes = {" 1.Endless\n", " 2.Quest\n", " 3.Help and Info\n"};
+  //Help and Information
   
-  //give player  basic information if they need it
-  std::cout << " 3.Info\n";
-
+  //Provides the player with the main menu
+  what_mode();
+  print_menu(modes);
   //gets player answer
-  std::cout << "enter the number of the option you choose: \n";
-  std::cin >> playerAnswer;
-
+  
+  
   //Displays custom message based on playe choice
-
   //sets mode to infinitee
-  if(playerAnswer == 1)
+  if(choice == 1)
   {
     infinite = true;
   }
 
   //sets mode to quest
-  else if(playerAnswer == 2)
+  else if(choice == 2)
   {
     quest = true;
   }
 
   //sends player to  information screen
-  else if(playerAnswer == 3)
+  else if(choice == 3)
   {
-    std::cout << "This is a text based adevture game where you enter the number of choice you wish to select. \n";
-    std::cout << "The only ting saved betwenn adventure is the gold you had upon your return and the unlocks you had";
-    std::cout << "You have the current adventuring options unlocked: \n";
-    std::cout << " 1.Endless\n";
-    std::cout << " 2.Quest\n";
-    std::cout << "In The Endless adeventure you will be able to  embark on an adventure endlessly with numerous senarios try to obtain the highest score you can by staying alive to tell the tale";
-    std::cout << "In The Quest mode you will embark on an adeventure where you recieve a quest and be able to complete the quest by completing quest goals the more you complete the more youll unlock";
-    std::cout << "If you are finshed reading enter 1 to continue back to the orginal menu";
-    std::cin >> playerAnswer;
-    
+    //provides basic explenation of the game
+    help();
+    choice = player_option();
     //Return player to main menu
-    if(playerAnswer == 1)
+    if(choice == 1)
     {
-      std::cout << "How Do you wish to adventure: \n";
-
-      //sets adventure mode
-      std::cout << " 1.Endless\n";
-      std::cout << " 2.Quest\n";
-
-      //gives player  basic information if they need it
-      std::cout << " 3.Info\n";
-
+      what_mode();
+      print_menu(modes);
       //gets player answer
-      std::cout << "enter the number of the option you choose: \n";
-      std::cin >> playerAnswer;
+      choice = player_option();
     }
       
     //Displays Error message and Re-deisplays previous message upon invalid selection
     else
     {
-      while(playerAnswer != 1)
+      while(choice != 1)
         {
-          std::cout << "ERROR It appears you entered an invalid option please try again.\n";
-          std::cout << "\nThis is a text based adevture game where you enter the number of choice you wish to select. \n";
-          std::cout << "The only ting saved betwenn adventure is the gold you had upon your return and the unlocks you had";
-          std::cout << "You have the current adventuring options unlocked: \n";
-          std::cout << " 1.Endless\n";
-          std::cout << " 2.Quest\n";
-          std::cout << "In The Endless adeventure you will be able to  embark on an adventure endlessly with numerous senarios try to obtain the highest score you can by staying alive to tell the tale";
-          std::cout << "In The Quest mode you will embark on an adeventure where you recieve a quest and be able to complete the quest by completing quest goals the more you complete the more youll unlock";
-          std::cout << "If you are finshed reading and uderstand enter 1 to continue back to the orginal menu";
-          std::cin >> playerAnswer;
+          error();
+          what_mode();
+          print_menu(modes);
+          choice = player_option();
         }
     } 
   }
   //Displays Error message and Re-deisplays previous message upon invalid selection
   else
   {
-    while(playerAnswer != 1 && playerAnswer != 2 && playerAnswer != 3)
+    while(choice != 1 && choice != 2 && choice != 3)
     {
-      std::cout << "ERROR It appears you entered an invalid option please try again.\n";
-      std::cout << "\nHow Do you wish to adventure: \n";
-      std::cout << " 1.Endless\n";
-      std::cout << " 2.Quest\n";
-      std::cout << " 3.Info\n";
-      std::cout << "enter the number of the option you choose: \n";
-      std::cin >> playerAnswer;
+      error();
+      what_mode();
+      print_menu(modes);
+      choice = player_option();
     }
   }
 
